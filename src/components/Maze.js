636559c95fb9c01e8af5ff5f4a73446eec14adf9
Manuel Arguelles <marguelles.dev@gmail.com>
2020-01-01 13:03:51 +0700

    moved tile model to independent dir
    Signed-off-by: Manuel Arguelles <marguelles.dev@gmail.com>

--- src/components/Maze.js
+++ src/components/Maze.js
@@ -1,130 +1,9 @@
 import React from 'react';
 import Tile from './Tile';
-import { Directions, TileTypes, UP, DOWN, LEFT, RIGHT } from '../utils/constants';
+import TileModel from '../models/TileModel';
+import { Directions, TileTypes } from '../utils/constants';
 import { shuffleArray, popFromSet } from '../utils/utils';
 import './Maze.scss';
-
-
-class TileObject {
-
-    connections = 0;
-    row = 0;
-    col = 0;
-    type = null;
-    isLocked = false;
-
-    constructor(row, col) {
-        this.row = row;
-        this.col = col;
-        this.type = TileTypes.WIRE;
-        this.connections = 0;
-        this.isLocked = false;
-    }
-
-    reset() {
-        this.type = TileTypes.WIRE;
-        this.connections = 0;
-        this.isLocked = false;
-    }
-
-    get isConnected() {
-        return this.type === TileTypes.SERVER || this._isConnected;
-    }
-
-    set isConnected(value) {
-        this._isConnected = value;
-    }
-
-    /** return the number of connections for all of the 4 directions */
-    getNumberConnections() {
-        let connections = [];
-        for (let dir of Directions) {
-            if (dir & this.connections)
-                connections.push(dir);
-        }
-        return connections.length;
-    }
-
-    neighborAt(direction, tiles) {
-        let row = this.row;
-        let col = this.col;
-        if (direction === UP)
-            row--;
-        else if (direction === DOWN)
-            row++;
-        else if (direction === LEFT)
-            col--;
-        else if (direction === RIGHT)
-            col++;
-        else
-            throw new Error(`invalid direction: ${direction}`);
-
-        return tiles.find(tile => {
-            return tile.row === row && tile.col === col;
-        });
-    }
-
-    static getOpposite(direction) {
-        let opposite = direction * 4;
-        if (direction === 9)
-            opposite = 6;
-        else if (opposite > 8)
-            opposite = opposite / 16;
-        return opposite;
-    }
-
-    hasFreeNeighbors(tiles) {
-        let tile = null;
-        for (let direction of Directions) {
-            tile = this.neighborAt(direction, tiles);
-            if (tile && tile.connections === 0) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    updateType() {
-        if (this.type !== TileTypes.SERVER && Directions.includes(this.connections)) {
-            this.type = TileTypes.TERMINAL;
-        }
-    }
-
-    rotateCW() {
-        if (this.isLocked)
-            return;
-
-        let newConnections = this.connections << 1;
-        if (newConnections > 15)
-            newConnections -= 15;
-        this.connections = newConnections;
-    }
-
-    rotateCCW() {
-        if (this.isLocked)
-            return;
-
-        let newConnections = this.connections >> 1;
-        if (this.connections & UP)
-            newConnections += LEFT;
-        this.connections = newConnections;
-    }
-
-    toProps() {
-        return {
-            col: this.col,
-            row: this.row,
-            type: this.type,
-            connections: this.connections,
-            isConnected: this.isConnected,
-            isLocked: this.isLocked
-        };
-    }
-
-    getKey() {
-        return `${this.row}-${this.col}`;
-    }
-}
 
 
 class Maze extends React.Component {
@@ -161,7 +40,7 @@ class Maze extends React.Component {
         const tiles = [];
         for (let row = 0; row < rows; row++) {
             for (let col = 0; col < cols; col++) {
-                tiles.push(new TileObject(row, col));
+                tiles.push(new TileModel(row, col));
             }
         }
         this.state.tiles = tiles;
@@ -197,7 +76,7 @@ class Maze extends React.Component {
 
                 // connect tile and neighbor
                 selectedTile.connections += direction;
-                selectedNeighbor.connections = TileObject.getOpposite(direction);
+                selectedNeighbor.connections = TileModel.getOpposite(direction);
                 activeTiles.push(selectedNeighbor);
 
                 // filter out cells which can't have new connections
@@ -235,7 +114,7 @@ class Maze extends React.Component {
                 if (!(activeTile.connections & direction))
                     continue;
                 let neighbor = activeTile.neighborAt(direction, tiles);
-                if (neighbor && (neighbor.connections & TileObject.getOpposite(direction))) {
+                if (neighbor && (neighbor.connections & TileModel.getOpposite(direction))) {
                     if (!visitedTiles.has(neighbor)) {
                         toVisitTiles.add(neighbor);
                     }
---
+++ src/models/TileModel.js
@@ -0,0 +1,126 @@
+import { Directions, TileTypes, UP, DOWN, LEFT, RIGHT } from '../utils/constants';
+import Tile from '../components/Tile';
+
+
+class TileModel {
+
+    connections = 0;
+    row = 0;
+    col = 0;
+    type = null;
+    isLocked = false;
+
+    constructor(row, col) {
+        this.row = row;
+        this.col = col;
+        this.type = TileTypes.WIRE;
+        this.connections = 0;
+        this.isLocked = false;
+    }
+
+    reset() {
+        this.type = TileTypes.WIRE;
+        this.connections = 0;
+        this.isLocked = false;
+    }
+
+    get isConnected() {
+        return this.type === TileTypes.SERVER || this._isConnected;
+    }
+
+    set isConnected(value) {
+        this._isConnected = value;
+    }
+
+    /** return the number of connections for all of the 4 directions */
+    getNumberConnections() {
+        let connections = [];
+        for (let dir of Directions) {
+            if (dir & this.connections)
+                connections.push(dir);
+        }
+        return connections.length;
+    }
+
+    neighborAt(direction, tiles) {
+        let row = this.row;
+        let col = this.col;
+        if (direction === UP)
+            row--;
+        else if (direction === DOWN)
+            row++;
+        else if (direction === LEFT)
+            col--;
+        else if (direction === RIGHT)
+            col++;
+        else
+            throw new Error(`invalid direction: ${direction}`);
+
+        return tiles.find(tile => {
+            return tile.row === row && tile.col === col;
+        });
+    }
+
+    static getOpposite(direction) {
+        let opposite = direction * 4;
+        if (direction === 9)
+            opposite = 6;
+        else if (opposite > 8)
+            opposite = opposite / 16;
+        return opposite;
+    }
+
+    hasFreeNeighbors(tiles) {
+        let tile = null;
+        for (let direction of Directions) {
+            tile = this.neighborAt(direction, tiles);
+            if (tile && tile.connections === 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    updateType() {
+        if (this.type !== TileTypes.SERVER && Directions.includes(this.connections)) {
+            this.type = TileTypes.TERMINAL;
+        }
+    }
+
+    rotateCW() {
+        if (this.isLocked)
+            return;
+
+        let newConnections = this.connections << 1;
+        if (newConnections > 15)
+            newConnections -= 15;
+        this.connections = newConnections;
+    }
+
+    rotateCCW() {
+        if (this.isLocked)
+            return;
+
+        let newConnections = this.connections >> 1;
+        if (this.connections & UP)
+            newConnections += LEFT;
+        this.connections = newConnections;
+    }
+
+    toProps() {
+        return {
+            col: this.col,
+            row: this.row,
+            type: this.type,
+            connections: this.connections,
+            isConnected: this.isConnected,
+            isLocked: this.isLocked
+        };
+    }
+
+    getKey() {
+        return `${this.row}-${this.col}`;
+    }
+}
+
+export default TileModel;
